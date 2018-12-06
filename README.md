77d54b95981f8bce65e19c1bc3f5c2ab2257d314
zhuyan <yan.zhu01@bqjr.cn>
2018-12-06 16:28:53 +0800

    代码更新！

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mpvue
+# mpyl
 
 > A Mpvue project
 
--- src/main.js
+++ src/main.js
@@ -7,14 +7,4 @@ App.mpType = 'app'
 const app = new Vue(App)
 app.$mount()
 
-export default {
-  config: {
-        pages: [],
-        window: {
-            backgroundTextStyle: 'light',
-            navigationBarBackgroundColor: '#fff',
-            navigationBarTitleText: '第一个小程序',
-            navigationBarTextStyle: 'black'
-        }
-    }
-}
+

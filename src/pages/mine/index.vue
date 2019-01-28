2fead8b3ae4e6faeff74770fe27ac467fbff0ad6
childyi <yi@zhuyan.me>
2019-01-29 07:53:52 +0800

    代码提交！

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,5 +1,71 @@
 <template>
-  <web-view src="{{src}}"></web-view>
+  <div>
+
+
+
+    <div class="" style="background-color: #ffffff;padding: 25px 24px;border-bottom: 1px solid #F3F3F3;">
+      <div class="cus-row">
+        <div class="cus-row-col-6 fs-18-fc-2E3133 f-f-m v-a-m" style="">用户登录</div>
+        <div class="cus-row-col-6 v-a-m t-al-r">
+          <image class="v-a-m in-bl" src="/static/images/user_pic_nor@3x.png" mode="widthFix" style="width:60px;height: 60px"></image>
+          <image class="v-a-m in-bl" src="/static/images/icon_bnext_nor@3x.png" mode="widthFix" style="width:8px;height: 13px;margin-left: 8px;"></image>
+        </div>
+      </div>
+    </div>
+
+
+    <div style="padding: 0 16px;">
+    <div class="flower-bg m-t-10">
+        <div class="cus-row">
+          <div class="cus-row-col-6 fs-16-fc-ffffff f-f-m v-a-m" style="line-height: 24px;">花甲会员· ¥60/月</div>
+          <div class="cus-row-col-6 v-a-m t-al-r">
+            <span class="fs-12-fc-ffffff f-f-r in-bl v-a-m">更多优惠，服务更多！</span>
+            <image class="v-a-m in-bl" src="/static/images/user_icon_next_nor@3x.png" mode="widthFix" style="width:8px;height: 13px;margin-left: 8px;"></image>
+          </div>
+        </div>
+    </div>
+
+    <view  class="mine-list m-t-16">
+      <navigator open-type="navigate" url="/pages/mine/invited_code_list"  hover-class="none" style="padding: 19px 16px;">
+        <view class="cus-row">
+          <view class="cus-row-col-10 fs-16-fc-000000-m v-a-m">我的订单</view>
+          <view class="cus-row-col-2 t-al-r v-a-m">
+            <image src="/static/images/icon_bnext_nor@3x.png" mode="widthFix" style="width:8px;height: 13px;"></image>
+          </view>
+        </view>
+      </navigator>
+
+      <navigator open-type="navigate" url="/pages/mine/cash" hover-class="none"   style="padding: 19px 16px;">
+        <view class="cus-row">
+          <view class="cus-row-col-10 fs-16-fc-000000-m v-a-m">我的地址</view>
+          <view class="cus-row-col-2 t-al-r v-a-m">
+            <image src="/static/images/icon_bnext_nor@3x.png" mode="widthFix" style="width:8px;height: 13px;"></image>
+          </view>
+        </view>
+      </navigator>
+
+      <navigator open-type="navigate" url="/pages/mine/qrcode"  hover-class="none"  style="padding: 19px 16px;">
+        <view class="cus-row">
+          <view class="cus-row-col-10 fs-16-fc-000000-m v-a-m">我的红包</view>
+          <view class="cus-row-col-2 t-al-r v-a-m">
+            <image src="/static/images/icon_bnext_nor@3x.png" mode="widthFix" style="width:8px;height: 13px;"></image>
+          </view>
+        </view>
+      </navigator>
+
+
+      <navigator open-type="navigate" url="/pages/mine/team" hover-class="none" style="padding: 19px 16px;">
+        <view class="cus-row">
+          <view class="cus-row-col-10 fs-16-fc-000000-m v-a-m">优惠券</view>
+          <view class="cus-row-col-2 t-al-r v-a-m">
+            <image src="/static/images/icon_bnext_nor@3x.png" mode="widthFix" style="width:8px;height: 13px;"></image>
+          </view>
+        </view>
+      </navigator>
+
+    </view>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -45,5 +111,12 @@
     color: red;
     padding: 10px;
     text-align: center;
+  }
+
+  .flower-bg{
+    background-image: linear-gradient(-153deg, #CE388E 9%, #A30182 100%);
+    box-shadow: 0 2px 8px 0 rgba(191,25,132,0.34);
+    border-radius: 5px;
+    padding: 19px 16px;
   }
 </style>

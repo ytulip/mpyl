a0d882ac1f5de21c230fa37965ec0999202f7e6c
zhuyan <yan.zhu01@bqjr.cn>
2019-02-28 19:43:13 +0800

    社区管家

--- src/pages/community/index.vue
+++ src/pages/community/index.vue
@@ -3,7 +3,7 @@
       <div style="position:relative">
 
           <div style="margin-top:60px;left: 0;right: 0;">
-                <div class="info-panel" style="padding: 60px 24px 24px 24px;">
+                <div class="common-panel" style="padding: 60px 24px 24px 24px;">
                     <div  class="t-al-c fs-18-fc-000000-m m-t-16" style="color:#C50081">小花</div>
                     <div class="m-t-24 fs-14-fc-7E7E7E-r">这里是一段套餐介绍的，午餐晚餐，每餐标准为2荤2素搭配堂主食为米饭，进了比这里是描述，目前还没有文况来定，之后文字补充上</div>
                 </div>
